69ab9fe083d7c2dd659b393119fec68eabcc11df
mari17179 <mariya.v.n@yandex.ru>
2020-07-02 12:36:14 +0300

    + user photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    added_attrs = [:user_photo, :username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: [added_attrs, :user_photo]
+    devise_parameter_sanitizer.permit :account_update, keys: [added_attrs, :user_photo]
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
 
   attr_writer :login
 
+  has_one_attached :user_photo
+  after_commit :add_default_user_photo, on: %i[create update]
+
   validate :validate_username
 
   def validate_username
@@ -25,6 +28,29 @@ class User < ApplicationRecord
      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
        where(conditions.to_h).first
      end
+   end
+
+   def user_photo_thumbnail
+     if user_photo.attached?
+        user_photo.variant(resize: "150x150!").processed
+     else
+        '/default_user_photo.jpg'
+     end
+   end
+
+   private
+   def add_default_user_photo
+      unless user_photo.attached?
+        user_photo.attach(
+          io: File.open(
+            Rails.root.join(
+              'app', 'assets', 'images', 'default_user_photo.jpg'
+            )
+          ),
+          filename: 'default_user_photo.jpg',
+          content_type: 'image/jpg'
+        )
+      end
    end
 
 end
